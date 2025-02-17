359ed0f73c68f1945d1e0e963678d800dbb5fc67
SumanAcharya <sumanthegreat30@gmail.com>
2025-02-16 18:22:24 -0600

    Added description of transformers

--- gpt.py
+++ gpt.py
@@ -1,3 +1,9 @@
+# Transformer is a deep learning architecture that allows AI models to process and generate text efficiently.
+# Before transformers , AI used RNN models and LSTM models to generate text, which struggled with long sentences and were slow.
+# As these models used to process one word at a time, they needed more computation power and memory
+# Transformers models can process the entire sentence at once, which makes them faster and more efficient.
+# Transformers in a general sense is a compiler, whereas LSTM models and RNN models are interpreters.
+
 from transformers import pipeline
 
 generator = pipeline('text-generation', model='gpt2')
